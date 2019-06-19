1295ca17ed4ad148e39b77e1bc174470a827d163
USwampertor <idv16c.mmillan@uartesdigitales.edu.mx>
2019-06-18 20:39:50 -0500

    Changing the NPC module so it can be modified at will and changed so others can add their NPC at will

---
+++ Daemon 1/Assets/Swampy/diDialog.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public enum eDialogType
+{
+  NONE = 0,
+  GOSSIP,
+  PURCHASE,
+  SELDOM,
+  SYSTEM,
+  ITEM,
+}
+
+
+[System.Serializable]
+public class diDialog
+{
+  public int id;
+  public string m_name;
+  public eDialogType m_type;
+  [TextArea]
+  public string m_gossip;
+  public List<diDialog> m_options;
+  public diItem m_item;
+}
---
+++ Daemon 1/Assets/Swampy/diItem.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class diItem : MonoBehaviour
+{
+  Sprite m_sprite;
+  int m_ID;
+  string m_name;
+}
