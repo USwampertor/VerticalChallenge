e7ef788e98b7f47e714edda68b8f670c2b3f478d
Luis Pineda <idv17c.lpineda@gmail.com>
2019-06-19 10:44:50 -0500

    New assets and tiles updated

---
+++ Daemon 1/Assets/Scripts/Manager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public GameObject          m_welcomeImage;
+    AnimationClip              m_animation;
+    bool                       m_isActive;
+
+    private void Awake()
+    {
+        Cursor.visible = false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_isActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(StreamVideo.m_isSkipped && !m_isActive)
+        {
+            //Instantiate(m_welcomeImage, new Vector3(0.0f, -0.0f, 0.0f), Quaternion.identity);
+            //m_animation = m_welcomeImage.GetComponent<AnimationClip>();
+            //m_isActive = true;
+            //Cursor.visible = true;
+        }
+    }
+}
---
+++ Daemon 1/Assets/Scripts/StreamVideo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class StreamVideo : MonoBehaviour
+{
+    public VideoPlayer      m_video;
+    public static bool      m_isSkipped;
+
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            m_video.Stop();
+            m_isSkipped = true;
+        }
+    }
+}
