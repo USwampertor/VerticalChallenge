a5ba8402c7d242a6c1f7e9499d3f6e2783100dab
Luis Pineda <idv17c.lpineda@gmail.com>
2019-06-19 14:35:25 -0500

    Main Menu in development and more assets added

---
+++ Daemon 1/Assets/Scripts/Menu/StreamVideo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class StreamVideo : MonoBehaviour
+{
+  public VideoPlayer      
+  m_video;
+  public static bool      
+  m_isSkipped;
+  public static bool      
+  m_isRepeating;
+
+  private void 
+  Start(){
+    m_isSkipped = false;
+    m_video.Play();
+  }
+
+  void 
+  Update(){
+    if (Input.anyKey || !m_video.isPlaying){
+        m_video.Stop();
+        m_isSkipped = true;
+    }
+  }
+}
