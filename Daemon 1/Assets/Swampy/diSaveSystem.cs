27ec7fae29155b3c10f1d781914c3b26fe7a7880
USwampertor <idv16c.mmillan@uartesdigitales.edu.mx>
2019-06-22 00:15:55 -0500

    +Creation of Saving System, Profile and advancing in the UI of the player *diPlayerUI.cs +diSaveSystem.cs +diProfile.cs

---
+++ Daemon 1/Assets/Swampy/diSaveSystem.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class diSaveSystem
+{
+
+  private static string m_saveFolder = Application.persistentDataPath + "/Saves/";
+  private static string m_extension = ".data";
+  private static string m_extensionType = "*.data";
+
+  public static void SaveProfile() {
+
+    BinaryFormatter formatter = new BinaryFormatter();
+    string path = m_saveFolder;
+    FileStream file = new FileStream(path, FileMode.Create);
+
+    diProfile profile = new diProfile();
+
+    formatter.Serialize(file, profile);
+    file.Close();
+
+  }
+
+  public static void Initialize() {
+    if(!Directory.Exists(m_saveFolder)) { Directory.CreateDirectory(m_saveFolder); }
+  }
+
+  public static void CreateProfile(string newName) {
+    BinaryFormatter formatter = new BinaryFormatter();
+    string path = m_saveFolder;
+    FileStream file = new FileStream(path, FileMode.Create);
+
+    diProfile profile = new diProfile();
+
+    formatter.Serialize(file, profile);
+    file.Close();
+  }
+
+  public static List<string> GetProfiles() {
+
+    List<string> profiles = new List<string>();
+
+    DirectoryInfo directory = new DirectoryInfo(m_saveFolder);
+    FileInfo[] files = directory.GetFiles(m_extensionType);
+
+    foreach (FileInfo file in files) {
+      profiles.Add(file.Name);
+    }
+    return profiles;
+  }
+
+  public static diProfile LoadProfile(string name) {
+
+    string path = m_saveFolder + name + m_extension;
+
+    if(File.Exists(path)) {
+      BinaryFormatter formatter = new BinaryFormatter();
+      FileStream stream = new FileStream(path, FileMode.Open);
+
+      diProfile profile;
+      profile = formatter.Deserialize(stream) as diProfile;
+
+      return profile;
+    }
+    throw new System.Exception("Couldn't Load given profile. Either it has " +
+                               "been tampered with or it was corrupted during" +
+                               "a saving state");
+    
+
+  }
+
+}
