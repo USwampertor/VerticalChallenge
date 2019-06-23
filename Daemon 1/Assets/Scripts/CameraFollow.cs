a7d9281e5879e069a9117912a2a41ef7d9e1b376
Luis Pineda <idv17c.lpineda@gmail.com>
2019-06-22 19:01:40 -0500

    Camera implemented, DroppableObject in progress, player in progress

---
+++ Daemon 1/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject m_player;
+
+    void Start()
+    {
+        m_player.GetComponent<GameObject>();
+    }
+
+    void Update()
+    {
+        transform.position = m_player.transform.position;
+    }
+}
--- Daemon 1/Assets/Scripts/ScriptableObjects/Consumables/Potion.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Potion", menuName = "Consumable/Potion")]
-public class Potion : ScriptableObject{
-
-}
